c85d598c06bc275091b3b824d499703a81aca681
Malygod <setas321@gmail.com>
2024-01-09 06:50:42 -0300

    cambios menores

--- src/components/ataliaFM.tsx
+++ src/components/ataliaFM.tsx
@@ -2,9 +2,29 @@ import React from "react";
 
 export const AtaliaMF = () => {
     return (
-        <div className="w-[280px] h-[604px]">
+        <div className="overflow-hidden max-w-full hidden sm:hidden md:hidden lg:hidden xl:flex justify-evenly items-center self-center py-[100px] ">
             <img
-                className="fixed w-[280px] h-[604px] top-0 left-0 object-cover"
+                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
+                alt="Agendarvisita"
+                src="../src/img/sideNavegation.png"
+            />
+            <img
+                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
+                alt="Agendarvisita"
+                src="../src/img/propiedades.png"
+            />
+            <img
+                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
+                alt="Agendarvisita"
+                src="../src/img/mobileLogin.png"
+            />
+            <img
+                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
+                alt="Agendarvisita"
+                src="../src/img/agendarVisita.png"
+            />
+            <img
+                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
                 alt="Agendarvisita"
                 src="../src/img/mobileFooter.png"
             />
---
+++ src/components/bgForest.tsx
@@ -0,0 +1,18 @@
+export const Forest = () => {
+    return (
+        <div
+            className="flex bg-white h-[120px] w-full blur-[2px]"
+            style={{
+                backgroundImage: "url('../src/img/bosqueDifuso.jpg')",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundAttachment: 'fixed',
+                filter: 'saturate(0.7)',
+            }}
+        >
+            
+        </div>
+    );
+};
+
+export default Forest;
