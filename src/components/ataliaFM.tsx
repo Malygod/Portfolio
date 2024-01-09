82fea5885261660931673184b52c55d629a2284f
Malygod <setas321@gmail.com>
2024-01-09 02:01:31 -0300

    first section

---
+++ src/components/ataliaFM.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const AtaliaMF = () => {
+    return (
+        <div className="w-[280px] h-[604px]">
+            <img
+                className="fixed w-[280px] h-[604px] top-0 left-0 object-cover"
+                alt="Agendarvisita"
+                src="../src/img/mobileFooter.png"
+            />
+        </div>
+    );
+};
+
+export default AtaliaMF;
