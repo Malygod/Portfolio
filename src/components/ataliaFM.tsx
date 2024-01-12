08ffca20dd00dbd485e80451b8878542c41a6613
Malygod <setas321@gmail.com>
2024-01-12 08:51:50 -0300

    experience update

--- src/components/ataliaFM.tsx
+++ src/components/ataliaFM.tsx
@@ -5,37 +5,39 @@ import img3 from "../img/mobileLogin.png"
 import img4 from "../img/agendarVisita.png"
 import img5 from "../img/mobileFooter.png"
 
+const images = [img1, img2, img3, img4, img5];
+
+
+
 
 export const AtaliaMF = () => {
-    return (
-        <div className="overflow-hidden max-w-full hidden sm:hidden md:hidden lg:hidden xl:flex justify-evenly items-center self-center py-[100px] ">
-            <img
-                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
-                alt="Agendarvisita"
-                src= {img1.src}
-            />
-            <img
-                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
-                alt="Agendarvisita"
-                src= {img2.src}
-            />
-            <img
-                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
-                alt="Agendarvisita"
-                src= {img3.src}
-            />
-            <img
-                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
-                alt="Agendarvisita"
-                src= {img4.src}
-            />
-            <img
-                className="transform w-[210px] h-[453px] self-center blur-md hover:scale-125 border-4 rounded-xl hover:border-teal-950 hover:blur-0 transition-all duration-300 ease-in-out"
-                alt="Agendarvisita"
-                src= {img5.src}
-            />
-        </div>
-    );
+  return (
+    <div className="overflow-hidden w-full max-w-full flex justify-evenly items-center h-[600px]">
+      <img
+        className="transform w-[105px] hover:scale-125 border-2 rounded-xl blur-sm hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        alt="Agendarvisita"
+        src={img1.src}
+      />
+      <img
+        className="transform w-[140px] h-auto hover:scale-125 border-2 rounded-xl blur-sm hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        alt="Agendarvisita"
+        src={img2.src}
+      />
+      <img
+        className="transform w-[210px] h-auto hover:scale-125 z-30 border-2 rounded-xl  hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        alt="Agendarvisita"
+        src={img3.src}
+      />
+      <img
+        className="transform w-[140px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-20 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        alt="Agendarvisita"
+        src={img4.src}
+      />
+      <img
+        className="transform w-[105px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-10 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        alt="Agendarvisita"
+        src={img5.src}
+      />
+    </div>
+  );
 };
-
-export default AtaliaMF;
--- src/components/bgForest.tsx
+++ src/components/bgForest.tsx
@@ -3,13 +3,13 @@ import img from "../img/bosqueDifuso.jpg";
 export const Forest = () => {
     return (
         <div
-            className="flex bg-white h-[120px] w-full blur-[2px]"
+            className="flex bg-white h-[100px] xl:h-[150px] w-full blur-[2px]"
             style={{
-                backgroundImage: `url(${img.src})`,  // Use the specific property that contains the image URL
+                backgroundImage: `url(${img.src})`, 
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundAttachment: 'fixed',
-                filter: 'saturate(0.7)',
+                filter: 'brightness(1.1)',
             }}
         >
             
