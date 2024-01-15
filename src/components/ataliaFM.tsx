58edecf5cb560a45e82234b42d4d70aa6c4be387
Malygod <setas321@gmail.com>
2024-01-15 16:50:05 -0300

    atopecito

--- src/components/ataliaFM.tsx
+++ src/components/ataliaFM.tsx
@@ -4,36 +4,32 @@ import img3 from "../img/mobileLogin.png"
 import img4 from "../img/agendarVisita.png"
 import img5 from "../img/mobileFooter.png"
 
-const images = [img1, img2, img3, img4, img5];
-
-
-
 
 export const AtaliaMF = () => {
   return (
-    <div className="overflow-hidden h-[580px] w-full max-w-full flex justify-evenly items-center xl:h-[650px]">
+    <div className="overflow-hidden h-[580px] w-full max-w-full flex justify-evenly items-center xl:h-[650px] z-10">
       <img
-        className="transform w-[105px] hover:scale-125 border-2 rounded-xl blur-sm hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[105px] hover:scale-125 border-2 rounded-xl blur-sm hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img1.src}
       />
       <img
-        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img2.src}
       />
       <img
-        className="transform w-[210px] h-auto hover:scale-125 border-2 rounded-xl z-30 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[210px] h-auto hover:scale-125 border-2 rounded-xl z-30 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img3.src}
       />
       <img
-        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-20 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-20 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img4.src}
       />
       <img
-        className="transform w-[105px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-10 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[105px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-10 hover:border-teal-700 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img5.src}
       />
--- src/components/workoutsyncImg.tsx
+++ src/components/workoutsyncImg.tsx
@@ -5,35 +5,31 @@ import img3 from "../img/loginWS.jpg"
 import img4 from "../img/downhillWS.jpg"
 import img5 from "../img/dataformWS.jpg"
 
-
-
-
-
 export const WorkoutsyncImg = () => {
   return (
     <div className="overflow-hidden h-[580px] w-full max-w-full flex justify-evenly items-center xl:h-[650px]">
       <img
-        className="transform w-[105px] hover:scale-125 border-2 rounded-xl blur-sm hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[105px] hover:scale-125 border-2 rounded-xl blur-sm hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img1.src}
       />
       <img
-        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img2.src}
       />
       <img
-        className="transform w-[210px] h-auto hover:scale-125 z-30 border-2 rounded-xl hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[210px] h-auto hover:scale-125 z-30 border-2 rounded-xl hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img3.src}
       />
       <img
-        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-20 hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[150px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-20 hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img4.src}
       />
       <img
-        className="transform w-[105px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-10 hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out"
+        className="transform w-[105px] h-auto hover:scale-125 border-2 rounded-xl blur-sm z-10 hover:border-rose-800 hover:blur-0 transition-all duration-300 ease-in-out cursor-pointer"
         alt="Agendarvisita"
         src={img5.src}
       />
