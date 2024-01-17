cc6a5ad7b892002046b80c90d1e2453020bda451
Malygod <setas321@gmail.com>
2024-01-16 22:31:55 -0300

    fixes

--- src/components/ataliaFM.tsx
+++ src/components/ataliaFM.tsx
@@ -7,20 +7,20 @@ import img5 from "../img/mobileFooter.png"
 
 export const AtaliaMF = () => {
   return (
-    <div className="h-[650px] w-full max-w-full flex justify-center items-center z-50 overflow-x-hidden overflow-y-visible">
+    <div className=" h-[450px] xl:h-[650px] w-full max-w-full flex justify-center items-center z-50 overflow-x-hidden overflow-y-visible">
 
       <img
-        className="transform w-[250px] mt-10 h-auto -rotate-6 hover:scale-110 rounded-lg z-20 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
+        className="transform w-[160px] xl:w-[250px] mt-10 h-auto -rotate-6 hover:scale-110 rounded-lg z-20 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
         alt="Agendarvisita"
         src={img2.src}
       />
       <img
-        className="transform w-[250px] h-auto hover:scale-110 rounded-lg z-30 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
+        className="transform w-[160px] xl:w-[250px] h-auto hover:scale-110 rounded-lg z-30 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
         alt="Agendarvisita"
         src={img3.src}
       />
       <img
-        className="transform w-[250px] mt-10 h-auto rotate-6 hover:scale-110 rounded-lg z-20 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
+        className="transform w-[160px] xl:w-[250px] mt-10 h-auto rotate-6 hover:scale-110 rounded-lg z-20 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
         alt="Agendarvisita"
         src={img1.src}
       />
