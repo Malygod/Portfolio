2ec3eb595a0ad95cb30874d1cdcf35edf3069ee9
Malygod <setas321@gmail.com>
2024-01-19 01:09:25 -0300

    Fixes

--- src/components/ataliaFM.tsx
+++ src/components/ataliaFM.tsx
@@ -7,7 +7,7 @@ import img5 from "../img/mobileFooter.png"
 
 export const AtaliaMF = () => {
   return (
-    <div className=" h-[450px] xl:h-[650px] w-full max-w-full flex justify-center items-center z-50 overflow-x-hidden overflow-y-visible">
+    <div className=" h-[450px] xl:h-[650px] w-full max-w-full flex justify-center items-center z-50 overflow-hidden xl:overflow-visible">
 
       <img
         className="transform w-[160px] xl:w-[250px] mt-10 h-auto -rotate-6 hover:scale-110 rounded-lg z-20 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
