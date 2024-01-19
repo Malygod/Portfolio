ba26caf2178e6c75953b2537bed42f00a8e0eb9e
Malygod <setas321@gmail.com>
2024-01-19 01:21:38 -0300

    Fixes

--- src/components/workoutsyncImg.tsx
+++ src/components/workoutsyncImg.tsx
@@ -7,7 +7,7 @@ import img5 from "../img/dataformWS.jpg"
 
 export const WorkoutsyncImg = () => {
   return (
-    <div className="h-[450px] xl:h-[650px] w-full max-w-full flex justify-center items-center z-50 overflow-hidden xl:overflow-visible">
+    <div className="h-[440px] xl:h-[650px] w-full max-w-full flex justify-center items-center z-50 overflow-hidden xl:overflow-visible">
 
       <img
         className="transform w-[160px] xl:w-[250px] mt-10 h-auto -rotate-6 hover:scale-110 rounded-lg z-20 transition-all duration-300 ease-in-out cursor-pointer shadow-black shadow-2xl"
