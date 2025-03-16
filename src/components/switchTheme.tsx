c3e930e150c57a10fbb969a7423829d5ea54f7e8
Malygod <matiasscontact@gmail.com>
2025-03-16 03:23:08 -0300

    cambio nombre componente switch

--- src/components/switchTheme.tsx
+++ src/components/switchTheme.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Switch } from "@headlessui/react";
 
-export default function SwitchDark(): JSX.Element {
+export default function SwitchTheme(): JSX.Element {
   const [enabled, setEnabled] = useState(false);
 
   useEffect(() => {
